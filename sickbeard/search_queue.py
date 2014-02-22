963262ff2bdcd87f236a84876779ccc5e44d682c
Giovanni <gborri@libero.it>
2014-02-22 21:32:13 +0100

    Fix another small bug

--- sickbeard/search_queue.py
+++ sickbeard/search_queue.py
@@ -201,7 +201,7 @@ class RSSSearchQueueItem(generic_queue.QueueItem):
                 else:
 
                     myDB = db.DBConnection()
-                    sql_selection="SELECT show_name, tvdb_id, season, episode, paused FROM (SELECT * FROM tv_shows s,tv_episodes e WHERE s.tvdb_id = e.showid) T1 WHERE T1.paused = 0 and T1.episode_id IN (SELECT T2.episode_id FROM tv_episodes T2 WHERE T2.showid = T1.tvdb_id and T2.status in (?,?,?) and T2.season!=0 ORDER BY T2.season,T2.episode LIMIT 1) ORDER BY T1.show_name,season,episode"
+                    sql_selection="SELECT show_name, tvdb_id, season, episode, paused FROM (SELECT * FROM tv_shows s,tv_episodes e WHERE s.tvdb_id = e.showid) T1 WHERE T1.paused = 0 and T1.episode_id IN (SELECT T2.episode_id FROM tv_episodes T2 WHERE T2.showid = T1.tvdb_id and T2.status in (?,?,?,?) and T2.season!=0 ORDER BY T2.season,T2.episode LIMIT 1) ORDER BY T1.show_name,season,episode"
                     results = myDB.select(sql_selection, [common.SNATCHED, common.WANTED, common.SKIPPED, common.DOWNLOADABLE])
 
                     show_sk = [show for show in results if show["tvdb_id"] == sqlEp["showid"]]
