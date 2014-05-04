38478f7858c6de0b9ed836f340e44e9a4c91418b
Giovanni <gborri@libero.it>
2014-05-04 12:58:21 +0200

    Added Failed Status to updateWantedList,setEpisodeToArchived,setEpisodeToWanted procedure to traktWatchListChecker.py module

--- sickbeard/traktWatchListChecker.py
+++ sickbeard/traktWatchListChecker.py
@@ -26,7 +26,7 @@ from sickbeard import encodingKludge as ek
 from sickbeard import logger
 from sickbeard import helpers
 from sickbeard import search_queue
-from sickbeard.common import SNATCHED, SNATCHED_PROPER, DOWNLOADED, DOWNLOADABLE, SKIPPED, UNAIRED, IGNORED, ARCHIVED, WANTED, UNKNOWN
+from sickbeard.common import SNATCHED, SNATCHED_PROPER, DOWNLOADED, DOWNLOADABLE, SKIPPED, UNAIRED, IGNORED, ARCHIVED, WANTED, UNKNOWN, FAILED
 from common import Quality, qualityPresetStrings, statusStrings
 from lib.trakt import *
 
@@ -199,8 +199,8 @@ class TraktChecker():
 
 	myDB = db.DBConnection()
 
-	sql_selection="SELECT show_name, tvdb_id, season, episode, paused FROM (SELECT * FROM tv_shows s,tv_episodes e WHERE s.tvdb_id = e.showid) T1 WHERE T1.paused = 0 and T1.episode_id IN (SELECT T2.episode_id FROM tv_episodes T2 WHERE T2.showid = T1.tvdb_id and T2.status in (?,?) and T2.season!=0 and airdate is not null ORDER BY T2.season,T2.episode LIMIT 1) ORDER BY T1.show_name,season,episode"
-	results = myDB.select(sql_selection,[SKIPPED,DOWNLOADABLE])
+	sql_selection="SELECT show_name, tvdb_id, season, episode, paused FROM (SELECT * FROM tv_shows s,tv_episodes e WHERE s.tvdb_id = e.showid) T1 WHERE T1.paused = 0 and T1.episode_id IN (SELECT T2.episode_id FROM tv_episodes T2 WHERE T2.showid = T1.tvdb_id and T2.status in (?,?,?) and T2.season!=0 and airdate is not null ORDER BY T2.season,T2.episode LIMIT 1) ORDER BY T1.show_name,season,episode"
+	results = myDB.select(sql_selection,[SKIPPED,DOWNLOADABLE,FAILED])
 
 	for cur_result in results:
 
@@ -356,7 +356,7 @@ class TraktChecker():
         if epObj == None:
             return
         with epObj.lock:
-            if epObj.status not in (SKIPPED, DOWNLOADABLE):
+            if epObj.status not in (SKIPPED, DOWNLOADABLE, FAILED):
                 return
             logger.log(u"Setting episode s"+str(s)+"e"+str(e)+" of show " + show.name + " to archived")
 
@@ -372,7 +372,7 @@ class TraktChecker():
         if epObj == None:
             return
         with epObj.lock:
-            if epObj.status not in (SKIPPED, DOWNLOADABLE):
+            if epObj.status not in (SKIPPED, DOWNLOADABLE, FAILED):
                 return
             logger.log(u"Setting episode s"+str(s)+"e"+str(e)+" of show " + show.name + " to wanted")
             # figure out what segment the episode is in and remember it so we can backlog it
